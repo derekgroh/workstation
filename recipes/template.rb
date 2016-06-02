bf6e41e4d61f313ea8d83009b5b266aa8707a71e
Derek Groh <dgroh@softlayer.local>
2016-06-02 05:22:07 -0500

    remove *nix and add windows

--- recipes/template.rb
+++
@@ -1,5 +0,0 @@
-#
-# Cookbook Name:: workstation
-# Recipe:: template
-#
-# Copyright (c) 2015 The Authors, All Rights Reserved.
--- test/integration/source/serverspec/vim_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'spec_helper'
-
-describe command('vim --version') do
-  its(:stdout) { should match /VIM - Vi IMproved 7.4/ }
-  its(:stdout) { should match /Compiled by vagrant@source-/ }
-end
